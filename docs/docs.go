45c30a647dfce65cdec140bb4d3a835f4a05a796
erassyl.zholdas <erassyl.zholdas@ffin.mobi>
2025-02-16 15:04:52 +0500

    upd swag

--- docs/docs.go
+++ docs/docs.go
@@ -2975,7 +2975,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8000",
+	Host:             "157.90.227.125:8000",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "swagger API",
